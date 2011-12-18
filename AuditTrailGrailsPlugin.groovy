207655c81441a648f17b0447b9f275da619ee2e3
Konstantinos Kostarellis <kosta.grails@gmail.com>
2011-12-18 22:38:29 +0100

    added Spring Security Core as dependency (dependsOn) to the plugin (audit-trail is not working without it)

--- AuditTrailGrailsPlugin.groovy
+++ AuditTrailGrailsPlugin.groovy
@@ -4,8 +4,7 @@ class AuditTrailGrailsPlugin {
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3 > *"
     // the other plugins this plugin depends on
-    def dependsOn = [hibernate:"1.3 > *"]
-//'spring-security-core':"1.0.1 > *"]
+    def dependsOn = [hibernate:"1.3 > *", springSecurityCore:"1.0.1 > *"]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp",
