a2e6b200d2bf28a3866038f641304af498d68675
sudhir nimavat <sudhir@nimavat.me>
2017-11-15 18:14:05 +0530

    Configure snapshot publishing, refactor build

--- travis-build.sh
+++ travis-build.sh
@@ -6,18 +6,26 @@ rm -rf ./audit-trails-plugin/build
 rm -rf ./demo-app/build
 
 echo "### Running plugin tests ###"
-(cd ./audit-trail-plugin && ./gradlew clean check assemble --stacktrace)
+(cd ./audit-trail-plugin && ./gradlew clean check assemble --stacktrace --no-daemon)
 
 echo "### Running demo app tests ###"
-(cd ./audit-trail-plugin && ./gradlew clean check --stacktrace)
+(cd ./audit-trail-plugin && ./gradlew clean check --stacktrace --no-daemon)
 
-if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG == "9ci/grails-audit-trail" && $TRAVIS_PULL_REQUEST == 'false' ]]; then
-	echo "### publishing plugin Bintray"
-	(cd ./audit-trail-plugin && ./gradlew bintrayUpload)
+if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG == "9ci/grails-audit-trail" && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+
+    if [[ -n $TRAVIS_TAG ]]
+    then
+        echo "### publishing release to BinTray"
+        (cd ./audit-trail-plugin && ./gradlew bintrayUpload --no-daemon)
+    else
+         echo "### publishing snapshot"
+         (cd ./audit-trail-plugin && ./gradlew publish --no-daemon)
+    fi
 
 else
-  echo "Not on master branch, so not publishing"
+  echo "Not a Tag or Not on master branch, not publishing"
   echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
+  echo "TRAVIS_TAG: $TRAVIS_TAG"
   echo "TRAVIS_REPO_SLUG: $TRAVIS_REPO_SLUG"
   echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
 fi
